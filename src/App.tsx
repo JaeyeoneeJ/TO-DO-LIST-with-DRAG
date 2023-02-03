ba01cdf810fb36b531a22db7f23dd5733aadc122
jaeyeoneej <5ikve@naver.com>
2023-02-03 18:32:12 +0900

    feat: #0 drag시 배열 위치 재조정 23-02-03
    - onDragEnd에 setToDos를 넣고, 기존 toDos의 값을 복사함
    - 복사한 값을 splice 메서드를 통해 source index(기존에 있던 위치)의 위치에서 삭제
    - 복사한 값을 splice 메서드를 통해 destination index(도착한 위치)의 위치에 아무것도 삭제하지 않고 추가(추가한 값은 draggableId가 toDo 값이므로 이를 이용함)
    - setToDos의 return 값에 복사되어 변경된 toDos의 값을 넣음

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,17 @@ const Card = styled.div`
 
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = ({ destination, source }: DropResult) => {};
+  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+    if (!destination) return;
+    setToDos((oldToDos) => {
+      const toDosCopy = [...oldToDos];
+      // 1) Delete item on source.index
+      toDosCopy.splice(source.index, 1);
+      // 2) Put back the item on the destination.index
+      toDosCopy.splice(destination?.index, 0, draggableId);
+      return toDosCopy;
+    });
+  };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
