689b646e5c7caef2cf464e1aeeb6b3a5b5b38b77
jaeyeoneej <5ikve@naver.com>
2023-02-03 15:22:09 +0900

    test: #0 Recoil atom과 selector를 사용하여 minutes을 hours로 변환 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
+
 function App() {
-  return <div>hello</div>;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
+  );
 }
 
 export default App;
